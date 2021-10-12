2c1e11bc38f03964b47ee0d018b2583c66121246
akj1608 <akj1608@gamil.com>
2021-10-12 21:40:50 +0530

    moved from desktop to remote repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+import Title from './Title';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '110b70402fc1baa51f0a6146e9d241782e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+       
+          </div>
+        ) : null}
+        <Title />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+            
+          <Typography variant="body1" component="h2">
+            <br/>
+            <br/>
+            <br/>
+
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/kumar-abhishek-jha/"> Abhishek Kumar Jha</a> -
+            <a className={classes.link} href="https://github.com/akj1608"> Github</a>
+          </Typography>
+          
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './title.css';
+
+const Title = () => {
+    return (
+        <div>
+            <h1 className="title" >NewsDrive</h1>
+        </div>
+    )
+}
+
+export default Title;
